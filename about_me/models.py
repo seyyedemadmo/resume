8675bf44d1a0765ca092013734e7c18cf85596cb
SeyyedEmad <seyyedemadmo@gmail.com>
2022-12-19 21:40:27 +0330

    add django-jalali to project

--- about_me/models.py
+++ about_me/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class About(models.Model):
+    desc = models.TextField(max_length=5000, null=True, blank=True, help_text="متن مورد نظر")
+    updated_at = models.DateTimeField(auto_now=True, null=False, blank=False, help_text='اخرین تغیر')
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False, help_text='تاریخ ایجاد')
+    chosen = models.BooleanField(default=False, )
+
