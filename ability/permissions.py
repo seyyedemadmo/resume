1906e4ee08a7247ace4f76122adc7995035e2110
SeyyedEmad <seyyedemadmo@gmail.com>
2022-12-19 13:46:50 +0330

    fix permission bug for admin user

--- ability/permissions.py
+++ ability/permissions.py
@@ -4,6 +4,6 @@ from rest_framework import permissions
 class OnlyAdminCanCreate(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if (request.user.is_authenticated and request.user.is_admin) or view.action.upper() == 'LIST':
+        if (request.user.is_authenticated and request.user.is_superuser) or view.action == 'list':
             return True
         return False
